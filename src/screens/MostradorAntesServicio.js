d9a55d96c73d23ec9028aa8ed9098bdd412a8688
Cesar Loperena <cloperena@grupoeco.com.mx>
2021-11-04 18:37:27 -0700

    Se agrega solicitud de permisos a la camara desde Mostrador antes de serivicio

--- src/screens/MostradorAntesServicio.js
+++ src/screens/MostradorAntesServicio.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {
   Alert,
   SafeAreaView,
@@ -9,56 +9,87 @@ import {
   Image,
 } from 'react-native';
 import * as ImagePicker from 'react-native-image-picker';
-import {FilledButton} from '../components/Button';
+import { FilledButton } from '../components/Button';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {UserContext} from '../context/UserContext';
+import { UserContext } from '../context/UserContext';
 import axios from 'axios';
-import {BASE_URL} from '../config';
-import {EstatusContext} from '../context/EstatusContext';
+import { BASE_URL } from '../config';
+import { EstatusContext } from '../context/EstatusContext';
 import { CommonActions } from '@react-navigation/native';
+import { PermissionsAndroid } from 'react-native';
 
 
-export function MostradorAntesServicio({route, navigation}) {
+
+export function MostradorAntesServicio({ route, navigation }) {
   const [filePath, setFilePath] = useState(null);
   const [file64, setFile64] = useState();
   const [contentType, setContentType] = useState('');
   const [enviar, setEnviar] = useState(0);
   //AuthFlow
-  const {estado} = React.useContext(EstatusContext);
-  const {authFlow} = React.useContext(EstatusContext);
+  const { estado } = React.useContext(EstatusContext);
+  const { authFlow } = React.useContext(EstatusContext);
   const { idTienda, nombreTienda, idViaje } = route.params;
   const user = React.useContext(UserContext);
+  const [cameraAccess, setCameraAccess] = useState(false);
 
 
-
-  launchCamera = () => {
-    let options = {
-      maxWidth:1024,
-      maxHeight:768,
-      includeBase64: true,
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-    ImagePicker.launchCamera(options, (response) => {
-      console.log('Response = ', response);
-
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-        alert(response.customButton);
+  const requestCameraPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.CAMERA,
+        {
+          title: "App Camera Permission",
+          message:"App needs access to your camera "
+          buttonNeutral: "Ask Me Later",
+          buttonNegative: "Cancel",
+          buttonPositive: "OK"
+        }
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        console.log("Camera permission given");
+        setCameraAccess(true);
       } else {
-        const source = {uri: response.uri};
-        console.log('response', JSON.stringify(response));
-        setFilePath(response.assets[0].uri);
-        setFile64(response.assets[0].base64);
-        setContentType(response.assets[0].type);
+        console.log("Camera permission denied");
+        setCameraAccess(false);
       }
-    });
+    } catch (err) {
+      console.warn(err);
+      setCameraAccess(false);
+    }
+  };
+  launchCamera = () => {
+    if (cameraAccess) {
+      let options = {
+        maxWidth: 1024,
+        maxHeight: 768,
+        includeBase64: true,
+        storageOptions: {
+          skipBackup: true,
+          path: 'images',
+        },
+      };
+      ImagePicker.launchCamera(options, (response) => {
+        console.log('Response = ', response);
+
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+          alert(response.customButton);
+        } else {
+          const source = { uri: response.uri };
+          console.log('response', JSON.stringify(response));
+          setFilePath(response.assets[0].uri);
+          setFile64(response.assets[0].base64);
+          setContentType(response.assets[0].type);
+        }
+      });
+    }
+    else{
+      requestCameraPermission();
+    }
   };
 
   guardarImagen = async () => {
@@ -70,26 +101,25 @@ export function MostradorAntesServicio({route, navigation}) {
       idViaje: estado.IdViaje
     };
     console.log(`aqui llega pariente: ${BASE_URL}Tiendas/InsertImagen`, img);
-    try 
-    {
-        
+    try {
+
 
       await axios.post(`${BASE_URL}Tiendas/InsertImagen`, img).then((res) => {
         const result = res.data;
         let jsonMostradorResulto = JSON.parse(result);
 
-        authFlow.setEstatus(9, idTienda, user.IdUsuario, estado.IdViaje).then(authFlow.getEstatus(0,user.IdUsuario).then(
+        authFlow.setEstatus(9, idTienda, user.IdUsuario, estado.IdViaje).then(authFlow.getEstatus(0, user.IdUsuario).then(
           Alert.alert('Listo', 'Se ha guardado la imagen', [
             {
               text: 'Aceptar',
-              onPress: () =>(
-              navigation.navigate('FormularioEntrega', {idTienda: idTienda, nombreTienda: nombreTienda})),
+              onPress: () => (
+                navigation.navigate('FormularioEntrega', { idTienda: idTienda, nombreTienda: nombreTienda })),
               // onPress: ()=> (console.log('ESTAD0'), console.log(idViaje),  console.log(estado.IdViaje))
             },
           ])
 
         ));
-  
+
 
         console.log('Resultado');
         console.log(jsonMostradorResulto);
@@ -100,41 +130,43 @@ export function MostradorAntesServicio({route, navigation}) {
     }
   };
 
-//Este Este useEffect se detona cuando se modifica el estado del viaje
-useEffect(async () => {
-  console.log("PANTALLA");
-  console.log(estado);
-  //navega a la ultima pantalla en que se encontraba el usuario 
-  navigation.dispatch(
-    CommonActions.navigate({
-      name: estado.Modulo,
-      params: {
-        idTienda,
-        nombreTienda
-      },
-    })
-  )
-}, [estado]);
-
+  //Este Este useEffect se detona cuando se modifica el estado del viaje
+  useEffect(async () => {
+    console.log("PANTALLA");
+    console.log(estado);
+    //navega a la ultima pantalla en que se encontraba el usuario 
+    navigation.dispatch(
+      CommonActions.navigate({
+        name: estado.Modulo,
+        params: {
+          idTienda,
+          nombreTienda
+        },
+      })
+    )
+  }, [estado]);
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
   React.useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight: () => <Text style={{color: 'white', paddingHorizontal:15}}>{user.name}</Text>,
+      headerRight: () => <Text style={{ color: 'white', paddingHorizontal: 15 }}>{user.name}</Text>,
     });
   }, []);
   return (
     <SafeAreaView>
       <View style={styles.container}>
-      <View style={styles.header}>
+        <View style={styles.header}>
           {/* <Text> idTienda: {idTienda}</Text> */}
           {/* <Text>nombreTienda: {nombreTienda}</Text> */}
           <Text style={styles.headerText}>{nombreTienda}</Text>
         </View>
-        <Text style={{padding: 20, fontWeight: 'bold'}}>
-          Segundo paso: Al llegar a la tienda tomar foto con caracteristicas 
+        <Text style={{ padding: 20, fontWeight: 'bold' }}>
+          Segundo paso: Al llegar a la tienda tomar foto con caracteristicas
         </Text>
         <Text>Tome una foto antes de comenzar a surtir el exibidor</Text>
         <View style={styles.row}>
-          <Text style={{paddingRight: 8}}>Imagen mostrador:</Text>
+          <Text style={{ paddingRight: 8 }}>Imagen mostrador:</Text>
           <Icon
             name="camera"
             size={25}
@@ -147,8 +179,8 @@ useEffect(async () => {
           <Image
             resizeMode="cover"
             resizeMethod="scale"
-            style={{justifyContent: 'center', width: 100, height: 100}}
-            source={{uri: filePath}}></Image>
+            style={{ justifyContent: 'center', width: 100, height: 100 }}
+            source={{ uri: filePath }}></Image>
         </View>
       </View>
       <View style={styles.row}>
