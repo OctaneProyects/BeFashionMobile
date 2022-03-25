32935253b58dab995cdfba05cf6cd51fb1306ace
Fcarrillo-octane <fcarrillo@octanesystems.mx>
2022-03-25 16:31:35 -0700

    api pruebas y chaclist tienda boton

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 //PRODUCCION
-export const BASE_URL = 'https://apptrack.befashion.mx/AppTrackApi/api/'
+// export const BASE_URL = 'https://apptrack.befashion.mx/AppTrackApi/api/'
+export const BASE_URL =' https://portal.grupoeco.com.mx/BefashionApi/api/'
 //LOCAL
 // export const BASE_URL = 'http://localhost:63745/api/';
 export const AppVersion = 'v 1.4.5-merging'
